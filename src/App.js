7d722168cf9b66a1593c275f44c5ee1635c6956b
Sema Nur ekmekci <ekmekci.semanur@gmail.com>
2024-01-27 19:44:59 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,38 +1,21 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Sidebar from './components/sidebar/Sidebar.jsx'
-import Home from './components/home/Home.jsx'
-import About from './components/about/About.jsx'
-import Services from './components/services/Services.jsx'
-import Resume from './components/resume/Resume.jsx'
-import Portfolio from './components/portfolio/Portfolio.jsx'
-import Pricing from './components/pricing/Pricing.jsx'
-import Testimonials from './components/testimonials/Testimonials.jsx'
-import Blog from './components/blog/Blog.jsx'
-import Contact from './components/contact/contact.jsx'
+import HomePage from './pages/HomePage.jsx'
 
 
 
-
- 
 const App = () => {
   return (
     <>
     <Sidebar/>
-    
-    <main className="main">
-      <Home/>
-      <About/>
-      <Services/>
-      <Resume/>
-      <Portfolio/>
-      <Pricing/>
-      <Testimonials/>
-      <Blog/>
-      <Contact/>
-
-    </main>
-  
-    
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<HomePage/>} />
+        </Routes>
+      </div>
+    </Router> 
     </>
   )
 }
