26b9d6de850d4a3aac7bee51f22b33372b726d16
SemaEkmekci <ekmekci.semanur@gmail.com>
2024-06-18 17:21:46 +0300

    portfolio data updata

--- src/components/portfolio/Data.jsx
+++ src/components/portfolio/Data.jsx
@@ -32,33 +32,101 @@ import Library2 from "../../assets/library-otomation-2.png"
 import Library3 from "../../assets/library-otomation-3.png"
 import Library4 from "../../assets/library-otomation-4.png"
 import Library5 from "../../assets/library-otomation-5.png"
-
-
+import aimouse from "../../assets/aivirtualmouse.png"
+import oys from "../../assets/oys1.png"
+import oys2 from "../../assets/oys2.png"
+import oys3 from "../../assets/oys3.png"
+import oys4 from "../../assets/oys4.png"
+import isyeri_oe from "../../assets/isyeri_oe.png"
 
 const Data =[
     {
         id:1,
+        img: [isyeri_oe],
+        title: "NEÜ OE İşyeri Ortak Eğitim Sayfası",
+        details: [{
+            icon: "fa-regular fa-file-code",
+            title: "NEÜ Ortak Eğitim İşyeri Bilgi Bankası Sayfası",
+            desc: "Necmettin Erbakan Üniversitesi Mühendislik Fakültesi'nin ortak eğitim programı kapsamında anlaşmalı firmaların yer aldığı ve öğrencilere bilgi sağladığı bir web sayfasıdır. Şehre ve bölüme göre filtreleme ve arama seçenekleri sunar.",
+        },
+        {
+            icon: "fa-solid fa-code",
+            title: "Language: ",
+            desc: "HTML - CSS - Vanilla JS",
+        },
+        {
+            icon: "fa-brands fa-github",
+            title: "Web Sitesi Linki: ",
+            link: "https://erbakan.edu.tr/ortakegitim/sayfa/4002123/isyeri-bilgi-bankasi",
+        }
+    ]
+    },
+    {
+        id:1,
+        img: [oys, oys2, oys3, oys4],
+        title: "Öğrenci Yoklama Sistemi",
+        details: [{
+            icon: "fa-regular fa-file-code",
+            title: "Öğrenci Yoklama Sistemi: ",
+            desc: "Necmettin Erbakan Üniversitesi Bilgisayar Mühendisliği 6.dönem seçmeli dersi olan \"Gömülü Sistemler\" dersi final projesi için ESP32 kullanılarak geliştirilmiş öğrenci yoklama sistemidir.",
+        },
+        {
+            icon: "fa-solid fa-code",
+            title: "Language: ",
+            desc: "Arduino - ESP32 - React.js - Node.js - MSSQL",
+        },
+        {
+            icon: "fa-brands fa-github",
+            title: "Source Code: ",
+            link: "https://github.com/SemaEkmekci/Ogrenci_Yoklama_Sistemi",
+        }
+    ]
+    },
+    {
+        id:1,
+        img: [aimouse],
+        title: "OpenCv ile Sanal Mouse",
+        details: [{
+            icon: "fa-regular fa-file-code",
+            title: "OpenCv ile Sanal Mouse (AiVirtualMouse): ",
+            desc: "Necmettin Erbakan Üniversitesi Bilgisayar Mühendisliği 6.dönem seçmeli dersi olan \"Görüntü İşleme\" dersi final projesi için geliştirilmiş kamerada el görüntüsü ile mouse kontrolu uygulamasıdır.",
+        },
+        {
+            icon: "fa-solid fa-code",
+            title: "Language: ",
+            desc: "OpenCv - Django",
+        },
+        {
+            icon: "fa-brands fa-github",
+            title: "Source Code: ",
+            link: "https://github.com/SemaEkmekci/aiVirtualMouse_Django_Project",
+        }
+    ]
+    },
+    {
+
+        id:2,
         img: [TMEDETECT],
         title: "Tübitak-1002 TME DETECT",
         details: [{
             icon: "fa-regular fa-file-code",
             title: "TME DETECT: ",
-            desc: "\"Panoramik radyografiler ile temporomandibular eklem bölgesindeki dejeneratif            değişikliklerin tespit edilmesi\" için YoloV5 ile eğitilmiş yapay zeka modelinin            kulllanıcılar tarafından kullanılması için geliştirilmiştir.",
+            desc: "\"Panoramik radyografiler ile temporomandibular eklem bölgesindeki dejeneratif            değişikliklerin tespit edilmesi\" için YoloV5 ile eğitilmiş yapay zeka modelinin kulllanıcılar tarafından kullanılması için geliştirilmiştir.",
         },
         {
             icon: "fa-solid fa-code",
             title: "Language: ",
             desc: "HTML-CSS-JS-DJANGO-MYSQL",
         },
-        // {
-        //     icon: "fa-brands fa-github",
-        //     title: "Github: ",
-        //     desc: "",
-        // }
+        {
+            icon: "fa-brands fa-github",
+            title: "Video: ",
+            link: "https://drive.google.com/file/d/1MdFOGAf_j6v-B1R-ubwp7y4eRy6GfRew/view?usp=sharing",
+        }
     ]
     },
     {
-        id:2,
+        id:3,
         img: [Langman,Langman2,Langman3,Langman4,Langman5,Langman6],
         title: "H-Langman",
         details: [{
@@ -79,7 +147,7 @@ const Data =[
     ]
     },
     {
-        id:3,
+        id:4,
         img: [Hospital,Hospital2,Hospital3,Hospital4,Hospital5,Hospital6,Hospital7,Hospital8,Hospital9],
         title: "Hastane Randevu Sistemi",
         details: [{
@@ -100,7 +168,7 @@ const Data =[
     ]
     },
     {
-        id:4,
+        id:5,
         img: [FikrimVar,FikrimVar2,FikrimVar3,FikrimVar4,FikrimVar5,FikrimVar6,FikrimVar7,FikrimVar8,FikrimVar9,FikrimVar10],
         title: "Fikrim Var",
         details: [{
@@ -121,7 +189,7 @@ const Data =[
     ]
     },
     {
-        id:5,
+        id:6,
         img: [TEKNOFESTUYZ],
         title: "Teknofest - Ulaşımda Yapay Zeka",
         details: [{
@@ -138,7 +206,7 @@ const Data =[
     ]
     },
     {
-        id:6,
+        id:7,
         img: [FruitNinja],
         title: "Fruit-Ninja",
         details: [{
@@ -159,7 +227,7 @@ const Data =[
     ]
     },
     {
-        id:7,
+        id:8,
         img: [Library,Library2,Library3,Library4,Library5],
         title: "Kütüphane Otomasyonu",
         details: [{
@@ -180,7 +248,7 @@ const Data =[
     ]
     },
     {
-        id:8,
+        id:9,
         img: [Datatable, Datatable],
         title: "Datatable",
         details: [{
--- src/components/portfolio/PortfolioItem.jsx
+++ src/components/portfolio/PortfolioItem.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { Modal, ModalBody, ModalHeader, Row, Col } from 'reactstrap';
-import 'react-slideshow-image/dist/styles.css'
-import {Slide } from 'react-slideshow-image';
+import React, { useState, useEffect } from "react";
+import { Modal, ModalBody, ModalHeader, Row, Col } from "reactstrap";
+import "react-slideshow-image/dist/styles.css";
+import { Slide } from "react-slideshow-image";
 import "./portfolio.css";
 
 const PortfolioItem = ({ img, title, details }) => {
@@ -10,23 +10,21 @@ const PortfolioItem = ({ img, title, details }) => {
 
   useEffect(() => {
     const handleScroll = () => {
-      const portfolioItems = document.querySelectorAll('.portfolio-item');
+      const portfolioItems = document.querySelectorAll(".portfolio-item");
       const triggerBottom = window.innerHeight * 0.8;
 
       portfolioItems.forEach((item) => {
         const itemTop = item.getBoundingClientRect().top;
 
         if (itemTop < triggerBottom) {
-          item.classList.add('visible');
-        } else {
-          item.classList.remove('visible');
+          item.classList.add("visible");
         }
       });
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
-    return () => window.removeEventListener('scroll', handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   const toggleModal = () => {
@@ -34,18 +32,19 @@ const PortfolioItem = ({ img, title, details }) => {
   };
 
   return (
-    <div className={`portfolio-item ${visible ? 'visible' : ''}`}>
+    <div className={`portfolio-item ${visible ? "visible" : ""}`}>
       <img src={img[0]} alt="resim-1" className="portfolio-img" />
-      <div className='portfolio-hover' onClick={toggleModal}>
-        <h3 className='portfolio-title'>
-          {title}
-        </h3>
+      <div className="portfolio-hover" onClick={toggleModal}>
+        <h3 className="portfolio-title">{title}</h3>
       </div>
 
-      <Modal size="lg" isOpen={modal} toggle={() => setModal(!modal)} zIndex={0} >
-        <ModalHeader toggle={() => setModal(!modal)}>
-          {title}
-        </ModalHeader>
+      <Modal
+        size="lg"
+        isOpen={modal}
+        toggle={() => setModal(!modal)}
+        zIndex={0}
+      >
+        <ModalHeader toggle={() => setModal(!modal)}>{title}</ModalHeader>
         <ModalBody>
           <Row>
             <Col lg={12}>
@@ -54,8 +53,16 @@ const PortfolioItem = ({ img, title, details }) => {
                   <li className="modal-item" key={index}>
                     <div>
                       <span className="item-title">{title}</span>
-                      <span className='item-details'>{desc}</span>
-                      <span className='item-details'><a href={link} target="_blank" rel="noreferrer noopener">{link}</a></span>
+                      <span className="item-details">{desc}</span>
+                      <span className="item-details">
+                        <a
+                          href={link}
+                          target="_blank"
+                          rel="noreferrer noopener"
+                        >
+                          {link}
+                        </a>
+                      </span>
                     </div>
                   </li>
                 ))}
@@ -64,26 +71,27 @@ const PortfolioItem = ({ img, title, details }) => {
             {/* <Col lg={12}>
               <img src={img} alt="" className='modal-img' />
               <br />
-            </Col> */}
-           
-            <Col lg={12}>
-                <div className='slide-container'>
-                  <Slide autoplay={true} duration={2000}>
-                  {img.map((image, index) => (
-                    <img key={index} src={image} alt={`Proje ${index}`}   className='modal-img' />
-                    ))}
-                    <br />
-                  </Slide>
-                  
-                </div>
-            </Col>
-           
+           </Col> */}
 
+            <Col lg={12}>
+              <div className="slide-container">
+                <Slide autoplay={true} duration={2000}>
+                  {img.map((image, index) => (
+                    <img
+                      key={index}
+                      src={image}
+                      alt={`Proje ${index}`}
+                      className="modal-img"
+                    />
+                  ))}
+                </Slide>
+              </div>
+            </Col>
           </Row>
         </ModalBody>
       </Modal>
     </div>
   );
-}
+};
 
 export default PortfolioItem;
